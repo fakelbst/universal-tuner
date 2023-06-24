a7b57f6bbf29c051cb351b05e454007700f0af7c
Mao <myj226@gmail.com>
2023-06-24 21:11:27 +0800

    Init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import ThemeToggle from './lib/ThemeToggle.svelte'
+  let colorScheme = 'light'
+
+  function handleSwitch() {
+    colorScheme = colorScheme === 'light' ? 'dark' : 'light'
+  }
+</script>
+
+<main>
+  <ThemeToggle onChange={handleSwitch} theme={colorScheme} />
+  <p>UNDER CONSTRUCTION</p>
+</main>
+
+<style>
+</style>
---
+++ src/lib/ThemeToggle.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let theme
+  export let onChange
+</script>
+
+<button on:click={onChange} class="toggle">{theme}</button>
+
+<style>
+.toggle {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+</style>
